8f0f716e894202f9eb36cb577d3ed610be98aeab
Uday Kumar <udaysinghpagurkothi@gmail.com>
2024-11-17 22:58:21 +0530

    wecode

--- We-code 3/classWorkJs/logic.js
+++
@@ -1,8 +0,0 @@
-let n = parseInt(prompt("Enter First num "))
-let b = parseInt(prompt("Enter second num "))
-alert("The sum of "+n+ " and "+b+" is "+(n + b))
-let id=document.querySelector("h1");
-let u = document.getElementById("yes");
-u.style.color = "red";
-let body = document.body;
-body.style.backgroundColor="blue";
